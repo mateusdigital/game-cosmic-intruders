42d84823dd4cdd0b444a4fa27b1967500310e325
mateusdigital <hello@mateus.digital>
2025-08-15 15:42:26 -0300

    !new -- Add seperate scripts, rename _ "helper" scripts

---
+++ scripts/build-game-linux.ps1
@@ -0,0 +1,27 @@
+##----------------------------------------------------------------------------##
+##                               *       +                                    ##
+##                         '                  |                               ##
+##                     ()    .-.,="``"=.    - o -                             ##
+##                           '=/_       \     |                               ##
+##                        *   |  '=._    |                                    ##
+##                             \     `=./`,        '                          ##
+##                          .   '=.__.=' `='      *                           ##
+##                 +                         +                                ##
+##                      O      *        '       .                             ##
+##                                                                            ##
+##  File      : build-game-linux.ps1                                          ##
+##  Project   : cosmic_intruders                                              ##
+##  Date      : 2025-08-15                                                    ##
+##  License   : See project's COPYING.TXT for full info.                      ##
+##  Author    : mateus.digital <hello@mateus.digital>                         ##
+##  Copyright : mateus.digital - 2025                                         ##
+##                                                                            ##
+##  Description :                                                             ##
+##                                                                            ##
+##----------------------------------------------------------------------------##
+
+$ErrorActionPreference = "Stop"
+
+$cwd = (Get-Location).Path.Replace("\", "/");
+$wslPath = "$(wsl wslpath $cwd)"
+wsl bash -c "cd $wslPath && ./scripts/_build-game.sh --release --linux"
